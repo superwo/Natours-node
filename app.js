4c1cc87986d7ae2d7af68f3f2a433b734ba067bf
Radu <avadanii@tiscali.it>
2019-07-15 11:13:16 +0300

    error handling

--- app.js
+++ app.js
@@ -22,4 +22,22 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    messgae: err.message
+  });
+});
+
 module.exports = app;
