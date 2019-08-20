af0c907770424cb6a736aca4fcfe39e6e2eac40b
Radu <avadanii@tiscali.it>
2019-08-20 11:34:37 +0300

    security and sanitization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,6 +17,9 @@ router.patch(
   authController.updatePassword
 );
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
 router
   .route('/')
   .get(userController.getAllUsers)
