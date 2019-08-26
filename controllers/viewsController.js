8b76517991f894970877a08f5498c856fb50e202
Radu <avadanii@tiscali.it>
2019-08-26 15:17:15 +0300

    included mapbox

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,20 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
+  });
+
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+    title: `${tour.name} Tour`,
+    tour
+  });
+});
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account'
   });
 };
